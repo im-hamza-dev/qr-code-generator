683c585a30b15ee0da04738398eebdc51478c2ab
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-09-03 01:51:26 +0500

    feature: api added to scan user-info

--- qr-code-client/src/components/list-view/list-view.component.js
+++ qr-code-client/src/components/list-view/list-view.component.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QRCode from "react-qr-code";
+import axios from "axios";
 
 const ListView = () => {
   let qrCode = {
-    redirectUrl: "www.shapepowered.com",
+    redirectUrl: "http://localhost:5000/api/scan/5",
     title: "Test QR code",
     squareColor: "black",
     eyeColor: "white",
   };
+  useEffect(() => {
+    axios
+      .post(qrCode.redirectUrl)
+      .then((result) => {
+        console.log("result", result);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }, []);
   return (
     <div>
       <h1>{qrCode.title}</h1>
