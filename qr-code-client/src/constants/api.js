f6b01f6e00df2a128c875538528822ed65232dc8
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-09-04 14:41:58 +0500

    change port of service

--- qr-code-client/src/constants/api.js
+++ qr-code-client/src/constants/api.js
@@ -1,10 +1,10 @@
 // let baseUrl = "https://qr-code-generator-v2-api.vercel.app";
-// let baseUrl = "http://localhost:5000";
 let clientUrl = "https://qr-code-tracker-v2.vercel.app";
+
+// let baseUrl = "http://localhost:5000";
 // let clientUrl = "http://localhost:3000"
 
-
-let baseUrl = "http://23.21.71.157:5005";
+let baseUrl = "http://23.21.71.157:5000";
 
 
 
